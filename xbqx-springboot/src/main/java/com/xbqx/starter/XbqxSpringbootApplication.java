07956843ec86293d93ac3ca341b7a6003782fd63
MrGaoGb <862230476@qq.com>
2025-01-13 01:23:20 +0800

    springboot项目

---
+++ xbqx-springboot/src/main/java/com/xbqx/starter/XbqxSpringbootApplication.java
@@ -0,0 +1,16 @@
+package com.xbqx.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main方法启动
+ */
+@SpringBootApplication
+public class XbqxSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XbqxSpringbootApplication.class, args);
+    }
+
+}
